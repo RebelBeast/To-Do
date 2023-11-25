e989a1e7b6746d4f8d2250c17c296c7ee0e16b1e
Maurice Trunk <maurice.trunk@live.de>
2023-11-25 07:59:43 +0100

    added todo editing methods

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -2,8 +2,11 @@
     <div>
         <input type="text" class="todo-input" placeholder="Was muss erledigt werden" v-model="newTodo" @keyup.enter="addTodo">
         <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-            <div>
-                {{ todo.title }}
+            <div class="todo-item-left">
+                <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">
+                    {{ todo.title }}
+                </div>
+                <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)">
             </div>
             <div class="remove-item" @click="removeTodo(index)">
                 &times;
@@ -24,11 +27,13 @@
                 'id': 1,
                 'title': 'Finish Vue Screen Cast',
                 'completed': false,
+                'editing': false,
             },
             {
-                'id': 1,
+                'id': 2,
                 'title': 'Take over the World',
                 'completed': false,
+                'editing': false,
             },
         ]
       }
@@ -49,6 +54,14 @@
             this.newTodo = ''
             this.idForTodo++
         },
+        editTodo(todo) {
+            todo.editing = true
+        },
+
+        doneEdit(todo) {
+            todo.editing = false
+        },
+
         removeTodo(index) {
             this.todos.splice(index, 1)
         }
@@ -83,6 +96,31 @@
 
   .remove-item:hover {
     color: black;
+  }
+
+  .todo-item-left {
+    display: flex;
+    align-items: center;
+  }
+
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  }
+
+  .todo-item-edit:focus {
+    outline: none;
   }
 
 
