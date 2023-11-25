fb85df7497799a1a929451865e1bb6538d23f392
Maurice Trunk <maurice.trunk@live.de>
2023-11-25 07:17:10 +0100

    added testing for empty todos + css

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -2,7 +2,12 @@
     <div>
         <input type="text" class="todo-input" placeholder="Was muss erledigt werden" v-model="newTodo" @keyup.enter="addTodo">
         <div v-for="todo in todos" :key="todo.id" class="todo-item">
-            {{ todo.title }}
+            <div>
+                {{ todo.title }}
+            </div>
+            <div class="remove-item">
+                &times;
+            </div>
         </div>
     </div>
   </template>
@@ -30,6 +35,11 @@
     },
     methods: {
         addTodo() {
+            if (this.newTodo.trim() === '') {
+                return
+            }
+
+
             this.todos.push({
                 id: this.idForTodo,
                 title: this.newTodo,
@@ -51,6 +61,23 @@
     font-size: 18px;
     margin-bottom: 16px;
   }
+
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+  }
+
+  .remove-item:hover {
+    color: black;
+  }
+
 
   </style>
   
